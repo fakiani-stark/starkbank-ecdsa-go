d8131e719f9c8c841a8ceea74af710a6fb57dcb0
vidalstark <matheus.vidal@starkbank.com>
2020-12-22 04:16:43 -0300

    Fix curves parameters

--- elipticcurve/curve.go
+++ elipticcurve/curve.go
@@ -58,10 +58,10 @@ func Secp256k1() CurveFp {
 	Name := "secp256k1"
 	A := big.NewInt(0)
 	B := big.NewInt(7)
-	P,_ := new(big.Int).SetString("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16)
-	N,_ := new(big.Int).SetString("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
-	Gx,_ := new(big.Int).SetString("0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16)
-	Gy,_ := new(big.Int).SetString("0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16)
+	P,_ := new(big.Int).SetString("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 0)
+	N,_ := new(big.Int).SetString("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 0)
+	Gx,_ := new(big.Int).SetString("0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 0)
+	Gy,_ := new(big.Int).SetString("0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 0)
 	Oid := []int64{1, 3, 132, 0, 10}
 
 	return CurveFp{
@@ -78,12 +78,12 @@ func Secp256k1() CurveFp {
 func Prime256v1() CurveFp {
 	Name := "prime256k1"
 	NistName := "P-256"
-	A,_ := new(big.Int).SetString("0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc", 16)
-	B,_ := new(big.Int).SetString("0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 16)
-	P,_ := new(big.Int).SetString("0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff", 16)
-	N,_ := new(big.Int).SetString("0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551", 16)
-	Gx,_ := new(big.Int).SetString("0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 16)
-	Gy,_ := new(big.Int).SetString("0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 16)
+	A,_ := new(big.Int).SetString("0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc", 0)
+	B,_ := new(big.Int).SetString("0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b", 0)
+	P,_ := new(big.Int).SetString("0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff", 0)
+	N,_ := new(big.Int).SetString("0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551", 0)
+	Gx,_ := new(big.Int).SetString("0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296", 0)
+	Gy,_ := new(big.Int).SetString("0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5", 0)
 	Oid := []int64{1, 2, 840, 10045, 3, 1, 7}
 
 	return CurveFp{
